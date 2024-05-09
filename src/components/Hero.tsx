a218bb41f180524ad810c4d965bb6039b1cc0b5d
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-05-09 11:27:34 -0400

    changed navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,12 +6,13 @@ const Hero = () => {
   return (
     <div className="relative min-h-screen">
       <Image
-        className="lg:w-[580px] xl:w-[620px] h-auto absolute right-0 top-0 "
+        className="lg:w-[580px] xl:w-[620px] h-auto absolute right-0 top-0 -z-10"
         src={"/hero2.png"}
         width={1000}
         height={600}
         alt="hero bg"
       />
+      {/* <Navbar /> */}
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,30 @@
 import Image from "next/image";
 import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div className="container pt-6">
-      <div className="flex justify-between items-center">
-        <Image src={"/logo.png"} width={50} height={50} alt="logo" />
-        <ul className="md:flex hidden gap-8 items-center font-semibold text-[14px]">
+    <div className="container pt-4">
+      <div className="flex justify-center items-center">
+        <Image
+          src={"/logo.png"}
+          width={50}
+          height={50}
+          alt="logo"
+          className="mr-4"
+        />
+        <ul className="md:flex hidden gap-8 items-center font-bold text-[14px] text-black">
           <li>Home</li>
           <li>About</li>
           <li>Food</li>
           <li>Dish</li>
           <li>Contact</li>
+          <button className="bg-accent text-white px-6 rounded-3xl">
+            Sign Up
+          </button>
         </ul>
+
+        <AiOutlineMenu className="md:hidden text-accent" size={30} />
       </div>
     </div>
   );
